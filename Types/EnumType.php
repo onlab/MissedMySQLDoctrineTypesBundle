f784c7670d6abca784de9dd738a8137a45c6ae89
Marcus Fernandez <mfernandez@rocket-internet.com>
2012-04-09 22:47:47 -0300

    Added Enum and Set Types

---
+++ Types/EnumType.php
@@ -0,0 +1,38 @@
+<?php
+namespace Rocket\ProcurementBundle\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Enum MySQL implementation
+ * 
+ * @author Marcus Fernandez <marcus at onlab dot org>
+ */
+class EnumType extends Type
+{
+    public function getName()
+    {
+        return "enum";
+    }
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'ENUM(' . $this->getSQLEnumDeclarationValues($fieldDeclaration['value']) . ')';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function getBindingType()
+    {
+        return \PDO::PARAM_STR;
+    }
+    
+    protected function getSQLEnumDeclarationValues(array $values)
+    {
+        return "'" . implode("','", $values) . "'";
+    }
+}
---
+++ Types/SetType.php
@@ -0,0 +1,52 @@
+<?php
+namespace Rocket\ProcurementBundle\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Set MySQL implementation
+ * 
+ * @author Marcus Fernandez <marcus at onlab dot org>
+ */
+class SetType extends Type
+{
+    public function getName()
+    {
+        return "set";
+    }
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'SET(' . $this->getSQLSetDeclarationValues($fieldDeclaration['values']) . ')';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        $values = array();
+        
+        $individualValue = strtok($value, ',');
+        while ($individualValue !== false) {
+            $values[] = $individualValue;
+            $individualValue = strtok(',');
+        }
+        
+        return $values;
+    }
+    
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return implode(',', $value);
+    }
+
+    public function getBindingType()
+    {
+        return \PDO::PARAM_STR;
+    }
+    
+    protected function getSQLSetDeclarationValues(array $values)
+    {
+        return "'" . implode("','", $values) . "'";
+    }
+}
